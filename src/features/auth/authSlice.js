b6cb1944765bc6dce6535c2fc965594f6c431ba5
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2023-01-04 17:15:52 +0600

    redux-store-firebase-config-implemented

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,40 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import auth from '../../firebase/firebase.config'
+const initialState = {
+    email: "",
+    role: "",
+    isLoading: true,
+    isError: false,
+    error: ""
+}
+
+export const createUser = createAsyncThunk('auth/createUser', async ({ email, password }) => {
+    const data = createUserWithEmailAndPassword(auth, email, password)
+    return data
+})
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    extraReducers: (builder) => {
+        builder.addCase(createUser.pending, (state) => {
+            state.isLoading = true;
+            state.isError = false;
+            state.error = "";
+        })
+        builder.addCase(createUser.fulfilled, (state, { payload }) => {
+            state.isLoading = false;
+            state.email = payload
+            state.isError = false;
+            state.error = "";
+        })
+        builder.addCase(createUser.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.email = "";
+            state.error = action.error.message;
+        })
+    }
+})
+
+export default authSlice.reducer
